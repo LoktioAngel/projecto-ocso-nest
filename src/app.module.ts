64b924b6519f9a01c3cb042e870148b0e4e91892
LokitoAngel <amgelgabo.d@gmail.com>
2024-09-28 22:51:31 -0600

    Problemas resueltos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,7 @@ import { AppService } from "./app.service";
 import { EmployeesModule } from "./employees/employees.module";
 import { ProductsModule } from "./products/products.module";
 import { ConfigModule } from "@nestjs/config";
-<<<<<<< HEAD
 import { ProvidersModule } from './providers/providers.module';
-=======
->>>>>>> 840a82138da515e5b222d4fb4442dbe8b9b5a232
 
 @Module({
   imports: [
@@ -26,10 +23,7 @@ import { ProvidersModule } from './providers/providers.module';
     }),
     EmployeesModule,
     ProductsModule,
-<<<<<<< HEAD
     ProvidersModule,
-=======
->>>>>>> 840a82138da515e5b222d4fb4442dbe8b9b5a232
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-<<<<<<< HEAD
       transform: true,
-=======
->>>>>>> 840a82138da515e5b222d4fb4442dbe8b9b5a232
     })
   );
   await app.listen(3000);

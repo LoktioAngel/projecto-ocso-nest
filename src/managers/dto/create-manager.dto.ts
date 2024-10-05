ed26f61757c69e69e22955807e9d6da5108e0e91
LokitoAngel <amgelgabo.d@gmail.com>
2024-10-04 19:47:10 -0600

    Manager and region setup

--- src/managers/dto/create-manager.dto.ts
+++ src/managers/dto/create-manager.dto.ts
@@ -1,1 +1,17 @@
-export class CreateManagerDto {}
+import { isStringObject } from "util/types";
+import { Manager } from "../entities/manager.entity";
+import { IsEmail, IsNumber, IsString, isString, MaxLength } from "class-validator";
+
+export class CreateManagerDto extends Manager{
+    @IsString()
+    @MaxLength(80)
+    managerFullName: string;
+    @IsString()
+    @IsEmail()
+    managerEmail: string;
+    @IsNumber()
+    managerSalary: number;
+    @IsString()
+    @MaxLength(16)
+    managerPhoneNumber: string;
+}
--- src/regions/entities/region.entity.ts
+++ src/regions/entities/region.entity.ts
@@ -1,1 +1,13 @@
-export class Region {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity()
+export class Region {
+   @PrimaryGeneratedColumn('increment')
+   regionId: number;
+   
+   @Column('text')
+   regionName: string;
+   
+   @Column('array')
+   regionStates: string[];
+}
